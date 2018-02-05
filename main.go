df53bc5285085df239a6c61c0a774282c760dc4e
Dmitriy Oprya <dmitriyopria@gmail.com>
2018-02-06 01:08:48 +0200

    update

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"github.com/go-chi/chi"
-	"net/http"
-	"encoding/json"
-	"os"
-	"fmt"
+	"bytes"
 	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
 	"io"
+	"net/http"
+	"os"
+	"strings"
 )
+
 type (
 	Query struct {
-		X int	`json:"x"`
-		Y int	`json:"y"`
+		X      int      `json:"x"`
+		Y      int      `json:"y"`
 		Images []string `json:"images"`
 	}
 	Response struct {
@@ -20,14 +25,16 @@ type (
 	}
 )
 
+func main() {
 
-func main (){
-	r := chi.NewRouter()
+	fileConverter("./kml.png")
+	//testFileConverter("./kml.converted")
+
+	/*r := chi.NewRouter()
 
 	r.Post("/", ImageHandler)
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3000", r)*/
 }
-
 
 func ImageHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -54,17 +61,41 @@ func ImageHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		fmt.Println("Can`t marshal json")
+		return
 	}
 
 	w.Write(buf)
 
 }
 
-func getPointValue(fileName string, queryX, queryY int) uint32{
+func getPointValue(fileName string, queryX, queryY int) (point uint32) {
+
+	if queryY < 0 || queryX < 0 {
+		fmt.Println("Invalid point coordinates")
+		return
+	}
+
 	file, err := os.OpenFile(fileName, os.O_RDONLY, 0644)
 
+	defer file.Close()
+
 	if err != nil {
-		fmt.Printf("Can`t read file : %s", err.Error())
+		fmt.Printf("Can`t read file : %s\n", err.Error())
+		return
+	}
+
+	image, err := png.Decode(file)
+
+	if err != nil {
+		fmt.Printf("Can`t read file, it`s not png : %s\n", err.Error())
+		return
+	}
+
+	rect := image.Bounds()
+
+	if rect.Max.Y < queryY || rect.Max.X < queryX {
+		fmt.Println("Invalid query point size")
+		return
 	}
 
 	var width, height uint32
@@ -72,12 +103,112 @@ func getPointValue(fileName string, queryX, queryY int) uint32{
 	binary.Read(file, binary.BigEndian, width)
 	binary.Read(file, binary.BigEndian, height)
 
-	offset := int64((queryY * int(height)) + queryX)
-
-	var point uint32
+	offset := int64((queryX * int(height)) + queryY)
 
 	file.Seek(offset, io.SeekCurrent)
-	
+
 	binary.Read(file, binary.BigEndian, point)
 	return point
+}
+
+func fileConverter(fileName string) {
+
+	file, err := os.OpenFile(fileName, os.O_RDONLY, 0644)
+
+	defer file.Close()
+
+	if err != nil {
+		fmt.Printf("Can`t read file : %s\n", err.Error())
+		return
+	}
+
+	image, err := png.Decode(file)
+
+	if err != nil {
+		fmt.Printf("Can`t read file, it`s not png : %s\n", err.Error())
+		return
+	}
+
+	rect := image.Bounds()
+
+	width := make([]byte, 4)
+	height := make([]byte, 4)
+
+	buf := bytes.Buffer{}
+
+	binary.BigEndian.PutUint32(width, uint32(rect.Max.X))
+	binary.BigEndian.PutUint32(height, uint32(rect.Max.Y))
+
+	fmt.Println(width)
+	fmt.Println(height)
+
+	buf.Write(width)
+	buf.Write(height)
+
+	for y := 0; y < rect.Max.Y; y++ {
+
+		for x := 0; x < rect.Max.X; x++ {
+
+			c, _ := color.NRGBAModel.Convert(rect.At(x, y)).(color.NRGBA)
+
+			pointByte := []byte{c.R, c.G, c.B, c.B}
+
+			fmt.Println(c.R, c.G, c.B, c.B)
+
+			buf.Write(pointByte)
+		}
+	}
+
+	newFile, err := os.Create(strings.TrimSuffix(fileName, ".png") + ".converted")
+
+	if _, err := newFile.Write(buf.Bytes()); err != nil {
+		fmt.Printf("Can`t write converted file: %s\n", err.Error())
+	}
+
+	newFile.Close()
+}
+
+func testFileConverter(fileName string) {
+
+	file, err := os.OpenFile(fileName, os.O_RDONLY, 0644)
+
+	defer file.Close()
+
+	if err != nil {
+		fmt.Printf("Can`t read file : %s\n", err.Error())
+		return
+	}
+
+	var width = make([]byte, 4)
+	var height = make([]byte, 4)
+
+	binary.Read(file, binary.BigEndian, width)
+	binary.Read(file, binary.BigEndian, height)
+
+	newRect := image.Rectangle{}
+	newRect.Max.X = int(binary.BigEndian.Uint32(width))
+	newRect.Max.Y = int(binary.BigEndian.Uint32(height))
+
+	im := image.NewNRGBA(newRect)
+
+	for y := newRect.Max.X; y > 0; y-- {
+		for x := newRect.Max.Y; x > 0; x-- {
+			buf := make([]uint8, 4)
+			if err := binary.Read(file, binary.BigEndian, buf); err != nil {
+				return
+			}
+			colour := color.NRGBA{R: buf[0], G: buf[1], B: buf[2], A: buf[3]}
+			im.Set(int(x), int(y), colour)
+		}
+	}
+
+	fileTest, err := os.Create("test.png")
+
+	if err != nil {
+		fmt.Println("Can`t create test file", err.Error())
+	}
+
+	png.Encode(fileTest, im)
+
+	fileTest.Close()
 }
